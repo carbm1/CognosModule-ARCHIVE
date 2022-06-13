28f70baffd2db9eb14adbe5ef77197b3b792720b
Craig Millsap <craig@carbm1.com>
2022-06-13 11:54:11 -0500

    Report Format Fix

--- CognosModule.psm1
+++ CognosModule.psm1
@@ -844,13 +844,19 @@ function Start-CognosReport {
     #Do not use UrlEncode here. The only character that must be encoded is the space so we have to use a Replace.
     $cognosfolder = $cognosfolder.Replace(' ','%20')
 
-    $downloadURL = "$($baseURL)/ibmcognos/bi/v1/disp/rds/outputFormat/path/$($cognosfolder)/$($report)/CSV?v=3&async=MANUAL"
+    switch ($extension) {
+        "pdf" { $rdsFormat = "PDF" }
+        "csv" { $rdsFormat = "CSV" }
+        "xlsx" { $rdsFormat = "spreadsheetML" }
+    }
 
-    Write-Verbose $downloadURL
+    $downloadURL = "$($baseURL)/ibmcognos/bi/v1/disp/rds/outputFormat/path/$($cognosfolder)/$($report)/$($rdsFormat)?v=3&async=MANUAL"
 
     if ($reportparams -ne '') {
         $downloadURL = $downloadURL + '&' + $reportparams
     }
+
+    Write-Verbose $downloadURL
 
     #Complex parameters require an XML file to work properly. The path must be specified when invoking this module.
     if ($XMLParameters -ne '') {
